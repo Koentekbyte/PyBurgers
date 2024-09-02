9f95e7f7797966bebf3500bb07b5bc8d56aba4b5
Koentekbyte <78475300+Koentekbyte@users.noreply.github.com>
2024-09-03 00:33:29 +0100

    Done 🤑🤑🤑
    Finally finished everything!!!

--- website/kitchenDisplay.py
+++ website/kitchenDisplay.py
@@ -1,7 +1,42 @@
-from flask import Blueprint
-
+from flask import Blueprint,render_template, redirect
+from .tables import Menu, OrderItems, Orders
+from . import db
 kitchenDisplay = Blueprint("kitchenDisplay",__name__)
 
 @kitchenDisplay.route("/")
-def menu():
-    return "View orders here"
+def kDisplay():
+   
+    orderDict = {}
+    db.session.commit()
+    orderIterator = Orders.query.filter_by(status="received").all()
+    for i in orderIterator:
+        relatedPackage = db.session.query(
+        OrderItems, Menu
+        ).filter(
+        OrderItems.orderID == i.orderID,
+        ).filter(
+        OrderItems.menuID == Menu.menuID,
+        ).all()
+
+        orderDict[i.orderID] = relatedPackage
+
+    #need to package all this into a dict, {1: }
+    """
+    1) iterate through received orders
+    2) use their orderID to find related orderItems and Menu records
+    3) add to dictionary as { 1 : [menu records, orderitems records], 2 : [menu records, orderitems records]}
+
+
+    """
+
+    return render_template("kitchen-display.html",orderDict=orderDict)
+
+@kitchenDisplay.route("/finished/<name>")
+def finished(name):
+    
+    db.session.commit()
+    toUpdate = Orders.query.filter_by(orderID=name, status="received").first()
+    toUpdate.status="finished"
+    db.session.commit()
+    return redirect("/kitchenDisplay")
+    
--- website/status.py
+++ website/status.py
@@ -1,7 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, render_template
+from .tables import OrderItems, Orders, Menu
+from . import db
 
 status = Blueprint("status",__name__)
 
 @status.route("/")
-def home():
-    return "View order status here"
+def sDisplay():
+    displayOrders = Orders.query.filter((Orders.status == 'received') | (Orders.status == 'finished')).all()
+
+
+    return render_template("statusBoard.html",displayOrders=displayOrders)
+@status.route("/collected/<name>")
+def collected(name):
+
+    orderToDelete=Orders.query.filter_by(orderID=name,status="finished").first()
+    itemsToDelete=OrderItems.query.filter_by(orderID=orderToDelete.orderID).all()
+
+    for i in itemsToDelete:
+        db.session.delete(i)
+    db.session.delete(orderToDelete)
+    db.session.commit()
+
+    return redirect("/status")
